1363d8f1ddc929b3fc6b58a3407afc3342e78222
ssskiv <ivanioshpa@gmail.com>
2021-01-28 20:06:02 +0300

    cpp commit

--- vkbot.cpp
+++ vkbot.cpp
@@ -3,12 +3,21 @@
 vkbot::vkbot(/*QWidget *parent*/)
 {
 QWidget w;
-QBoxLayout layout(QBoxLayout::LeftToRight,&w);
+QVBoxLayout layout(&w);
 pal.setColor(QPalette::Window, Qt::black);
 pal.setColor(QPalette::WindowText, Qt::green);
 pal.setColor(QPalette::Button, Qt::darkCyan);
 pal.setColor(QPalette::Window, Qt::blue);
 w.setPalette(pal);
 w.setAutoFillBackground(true);
+Button *vk=createButton("VK",SLOT(vkClicked()));
+layout.addWidget(vk);
+setLayout(&layout);
 }
-void vkbot::vkclicked(){}
+void vkbot::vkClicked(){}
+Button *vkbot::createButton(const QString &text, const char *member)
+{
+    Button *button = new Button(text);
+    connect(button, SIGNAL(clicked()), this, member);
+    return button;
+}
--- vkbot.h
+++ vkbot.h
@@ -1,6 +1,6 @@
 #ifndef VKBOT_H
 #define VKBOT_H
-
+#include "button.h"
 #include <QObject>
 #include <QPalette>
 #include <QtWidgets>
@@ -9,12 +9,12 @@ class vkbot: public QWidget
 {
     Q_OBJECT
     QPalette pal;
-
+    Button *createButton(const QString &text, const char *member);
 
 public:
     vkbot(/*QWidget *parent = nullptr*/);
 private slots:
-    void vkclicked();
+    void vkClicked();
 };
 
 #endif // VKBOT_H
