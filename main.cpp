233839d1108b8385f393a3eb51465b2ebcbf6ce0
ssskiv <ivanioshpa@gmail.com>
2021-01-28 18:57:35 +0300

    cpp commit

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
 #include <QApplication>
 #include <QtWidgets>
 #include <QPalette>
-void cprint(QString text);
+#include "longpoll.h"
+void cprint(QString text,QLabel* cont);
+QPushButton *createButton(const QString &text, const char *member, QObject* th);
+void clicked();
 
 
-QTabWidget tabs;
-QWidget vktab;
-QWidget contab;
-QPalette vk;
-QPalette console;
-QPushButton tfrstrtbt(&vktab);
-QLabel cont(&contab);
+
 QString ct;
+bool cld;
+
 int main(int argc, char *argv[])
 {
-QApplication a(argc, argv);
-QWidget w;
-QHBoxLayout layout(&w);
+    QApplication a(argc, argv);
+    QWidget w;
+    QHBoxLayout layout(&w);
+    QTabWidget tabs;
+    QWidget vktab;
+    QWidget contab;
+    QPalette vk;
+    QPalette console;
+    //QPushButton tfrstrtbt(&vktab);
+    QPushButton tfrstrtbt= createButton("",&vktab,);
+    QLabel cont(&contab);
     ct="dfsjlkfbsdksdjfgbslkjdfhbskljfhbskldjfbskjfsbdkljfbskjdf\n";
     cont.setText(ct);
     ct+="dfsjlkfbsdksdjfgbslkjdfhbskljfhbskldjfbskjfsbdkljfbskjdf\n";
@@ -38,13 +45,28 @@ QHBoxLayout layout(&w);
     tfrstrtbt.setAutoFillBackground(true);
     tabs.addTab(&vktab,"VK");
     tabs.addTab(&contab,"Console");
-cprint("shalabola");
+    cprint("App started",&cont);
+    if(cld){
+    cprint("clicked",&cont);
+    cld=false;
+    }
     w.show();
     return a.exec();
 
 }
-void cprint(QString text)
+void cprint(QString text,QLabel* cont)
 {
-    ct+=text+'\n';
-    cont.setText(ct);
+    QDateTime tm(QDateTime::currentDateTime());
+    ct+=tm.toString(Qt::SystemLocaleShortDate)+": "+text+'\n';
+    cont->setText(ct);
+}
+QPushButton *createButton(const QString &text, const char *member, QObject* th)
+{
+    QPushButton *button = new QPushButton(text);
+    QObject::connect(button, SIGNAL(clicked()), th, member);
+    return button;
+}
+void clicked()
+{
+cld=true;
 }
